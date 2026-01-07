cc47b5655687aef4640d335cafc3812b3e72f48f
JISU KANG <162910117+chasyu0@users.noreply.github.com>
2026-01-07 23:46:12 +0900

    [Bronze II] Title: 소수 찾기, Time: 96 ms, Memory: 12936 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         
+         int N = sc.nextInt();
+         int count = 0;
+         
+         for(int i=0; i<N; i++) {
+             int num = sc.nextInt();
+             if(num<2) continue;
+             boolean isPrime = true;
+         
+         for(int j=2; j*j<=num; j++) {
+             if(num % j == 0) {
+                 isPrime = false;
+                 break;
+             }
+         }
+         
+         if (isPrime) {
+             count++;
+         }
+     }
+         
+    System.out.println(count);
+    sc.close();
+    }
+}
