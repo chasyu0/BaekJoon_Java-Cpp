cbc78ff91001558923f93f5eb9e9dd15f718f2e9
JISU KANG <162910117+chasyu0@users.noreply.github.com>
2026-01-13 14:25:45 +0900

    [Bronze II] Title: 분해합, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int M = 0;
+
+        for(int i=0; i<N; i++) {
+            int num = i; 
+            int sum = 0;
+            
+            while(num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            
+            if(sum + i == N) {
+                M = i;
+                break;
+            }
+        }
+        
+        System.out.println(M);
+        sc.close();
+    }
+}
